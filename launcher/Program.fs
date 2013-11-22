fb2f30c86f71db644f50cc6f54e0431096defa87
Nicolas Rolland <nicolas.rolland@gmail.com>
2013-11-22 14:13:43 +0100

    added a project for debugging my compile script along with FSharp.Formatting

---
+++ launcher/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System.Reflection
+
+[<EntryPoint>]
+let main argv = 
+    FormattingXQuant.generateDocs() 
+    printfn "%A" argv
+    0 // return an integer exit code
